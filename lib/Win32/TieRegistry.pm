b03df43fa4c029db45eb286971c6fc72eeccaa53
Alexandr Ciornii <alexchorny@gmail.com>
2015-04-27 13:35:07 +0300

    version 0.29_02

--- lib/Win32/TieRegistry.pm
+++ lib/Win32/TieRegistry.pm
@@ -15,7 +15,7 @@ use Tie::Hash ();
 use vars qw( $PACK $VERSION @ISA @EXPORT @EXPORT_OK );
 BEGIN {
 	$PACK    = 'Win32::TieRegistry';
-	$VERSION = '0.29_01';
+	$VERSION = '0.29_02';
 	@ISA     = 'Tie::Hash';
 }
 
